477671515aed6e724583e5e113ca0efce9e11b30
Devsebastian44 <zhunaulasebastian31@gmail.com>
2026-03-09 13:20:27 -0500

    style: fix Flake8 linting violations

--- tests/test_tracker.py
+++ tests/test_tracker.py
@@ -1,9 +1,11 @@
 import unittest
 
+
 class TestTracker(unittest.TestCase):
     def test_dummy(self):
         """Dummy test para validar el pipeline (CI) en el laboratorio."""
         self.assertTrue(True)
+
 
 if __name__ == '__main__':
     unittest.main()
